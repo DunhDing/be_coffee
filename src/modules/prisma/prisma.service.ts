e792b814711a7f5d4f8d979e1498c96bc48aba74
thanhbinh2304 <binh2304tto@gmail.com>
2026-05-31 14:48:43 +0700

    Enhance PrismaService constructor to include SSL configuration for production environment

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -9,15 +9,20 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
   private pool: Pool;
 
   constructor() {
+    const isProduction = process.env.NODE_ENV === 'production';
+
     // 1. Khởi tạo một Connection Pool từ thư viện 'pg' sử dụng URL từ .env
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-    
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: isProduction ? { rejectUnauthorized: false } : false,
+    });
+
     // 2. Tạo một Prisma Adapter bọc quanh pool đó
     const adapter = new PrismaPg(pool);
 
     // 3. Truyền adapter vào hàm super() của PrismaClient
     super({ adapter });
-    
+
     this.pool = pool;
   }
 
